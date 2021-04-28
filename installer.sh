9b38f318ae04afeec81a6a7307c5c66929245fd5
Haptic-Lazier <83362171+Haptic-Lazier@users.noreply.github.com>
2021-04-28 14:15:42 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,11 +3,12 @@ pip3 install os
 pip3 install praw
 pip3 install shutil
 
+mkdir Haptic_viewer
+mkdir Haptic_viewer/pic_gif_output
 mkdir Haptic_viewer/pic_gif_output/Pictures
 mkdir Haptic_viewer/pic_gif_output/Gifs
 mkdir Haptic_viewer/pic_gif_output/Texts
 
-wget -qO Haptic_viewer/ https://github.com/Haptic-Lazier/Haptic-veiwer/blob/main/haptic_go.py
-wget -qO Haptic_viewer/ https://github.com/Haptic-Lazier/Haptic-veiwer/blob/main/cred.py
-
+mv haptic_go.py Haptic_viewer/
+mv cred.py Haptic_viewer/
 echo "Install Complete, type 'cd Haptic_viewer/cred.py' to configure the bot to get started!"
